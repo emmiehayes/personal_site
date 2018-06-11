ff159713dca17c5ad65a801decd3e9fb00ac6fd4
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-11 14:09:28 -0600

    build case to include about page, add about view and pass testt

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -14,4 +14,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?('Page not found.')
     assert_equal 404, page.status_code
   end
+
+  def test_user_can_see_the_about_page
+    visit '/about'
+
+    assert page.has_content?("Here's some stuff to know.")
+    assert_equal 200, page.status_code
+  end
 end
